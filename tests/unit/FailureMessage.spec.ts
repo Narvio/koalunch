c3fcd848db4ecdb135f9271022b8a2745f0d6baa
Michal Kozubik <michal.kozubik@sap.com>
2021-08-16 21:43:41 +0200

    Make the first unit test working

---
+++ tests/unit/FailureMessage.spec.ts
@@ -0,0 +1,13 @@
+import { expect } from "chai";
+import { shallowMount } from "@vue/test-utils";
+import FailureMessage from "@/components/utils/FailureMessage.vue";
+
+describe("FailureMessage.vue", () => {
+  it("renders message when passed", () => {
+    const message = "new message";
+    const wrapper = shallowMount(FailureMessage, {
+      props: { message },
+    });
+    expect(wrapper.text()).to.include(message);
+  });
+});
